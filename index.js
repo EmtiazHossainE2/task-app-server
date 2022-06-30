25bb1a63f5be33f67f33a539141b228e9085d92b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-06-30 22:23:32 +0600

    update done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const cors = require('cors');
@@ -20,12 +20,32 @@ async function run() {
         await client.connect();
         const todosCollection = client.db("todos").collection("todos");
 
-        //add todos 
+        //add todo 
         app.post('/addtodo', async (req, res) => {
             const todo = req.body
             const result = await todosCollection.insertOne(todo)
             res.send(result)
         })
+
+        //get all todos
+        app.get('/todos', async (req, res) => {
+            const query = {}
+            const todos = await todosCollection.find(query).toArray()
+            res.send(todos)
+        })
+
+        // update todo
+        app.patch('/todos/:id', async (req, res) => {
+            const id = req.params.id
+            const todoInfo = req.body
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: todoInfo
+            };
+            const result = await todosCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
 
     }
     finally {
